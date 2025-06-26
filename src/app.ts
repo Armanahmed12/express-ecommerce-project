5d28bbdda4fd02f949cc9e511768f583f5e5a1ae
Armanahmed12 <mdarmanahmed1232@gmail.com>
2025-06-26 18:04:24 +0600

    working on deployment

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,14 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import productRouter from "./app/modules/product/product.routes";
 import orderRouter from "./app/modules/order/order.routes";
+import config from "./app/config";
+import mongoose from "mongoose";
 const app: Application = express();
+
+// ✅ Connect to MongoDB here so Vercel can initialize it
+mongoose.connect(config.dbUrl as string)
+  .then(() => console.log("🟢 MongoDB connected successfully"))
+  .catch(err => console.log("🔴 MongoDB connection error", err));
 
 // middlewares
 app.use(cors());
@@ -13,6 +20,7 @@ app.use('/api/orders', orderRouter);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
 
 // 404 handler (must be after all routes)
 app.use((req: Request, res: Response) => {
